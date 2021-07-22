dc79ea0ce7dbab45c467148fd0edd4669068ed10
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-22 17:32:54 +0300

    added images to seed data from azure storage

---
+++ Mango.Services.ProductAPI/Migrations/20210722143458_ImageUrlsAddedToSeed.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    public partial class ImageUrlsAddedToSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://mangomicroservices.blob.core.windows.net/mango/14.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://mangomicroservices.blob.core.windows.net/mango/12.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://mangomicroservices.blob.core.windows.net/mango/11.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://mangomicroservices.blob.core.windows.net/mango/13.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
