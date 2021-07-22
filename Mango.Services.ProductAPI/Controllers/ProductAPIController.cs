33cf897fd6012d4fa75ec13c1b662ebde189ab70
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-22 17:08:51 +0300

    implemented controller endpoints for crud

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,108 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get([FromRoute] int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto productToCreate = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = productToCreate;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() }; ;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto productToCreate = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = productToCreate;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() }; ;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                _response.Result = await _productRepository.DeleteProduct(id);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() }; ;
+            }
+
+            return _response;
+        }
+    }
+}
