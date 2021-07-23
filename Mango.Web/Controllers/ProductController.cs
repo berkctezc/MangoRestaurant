dee463e7642162061b947acd3e865991af730cfb
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-23 23:15:19 +0300

    got identity server template insterted into mvc identity layer

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
