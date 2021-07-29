5109c1fb74e629a112c9cdcc05039196968515bc
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-29 23:39:12 +0300

    created dtos, context and migration of shopping cart

--- Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.DbContexts
 {
@@ -6,8 +7,9 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
-        //public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails{ get; set; }
     }
 }
---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDto
+    {
+        public CartHeader CartHeader { get; set; }
+        public IEnumerable<CartDetails> CartDetails { get; set; }
+    }
+}
