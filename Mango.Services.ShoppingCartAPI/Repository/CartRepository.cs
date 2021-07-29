d202df70bf6509853f0b25ff3cd0cd552df6d7b3
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-30 01:33:58 +0300

    shoppingcartapi cart repository create and update functionality

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -1,28 +1,81 @@
-﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+﻿using System.Linq;
+using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
 using System.Threading.Tasks;
+using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Repository
 {
     public class CartRepository : ICartRepository
     {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
         public async Task<bool> ClearCart(string userId)
         {
-            throw new System.NotImplementedException();
         }
 
         public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
         {
-            throw new System.NotImplementedException();
+            Cart cart = _mapper.Map<Cart>(cartDto);
+
+            var prodInDb = await
+                _db.Products.FirstOrDefaultAsync(u => u.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+            if (prodInDb == null)
+            {
+                _db.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _db.SaveChangesAsync();
+            }
+
+            var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking().FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
+
+            if (cartHeaderFromDb == null)
+            {
+                _db.CartHeaders.Add(cart.CartHeader);
+                await _db.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                var cartDetailsFromDb = await _db.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    u => u.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                         u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                if (cartDetailsFromDb == null)
+                {
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    cart.CartDetails.FirstOrDefault().Count += cartDetailsFromDb.Count;
+                    _db.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+            return _mapper.Map<CartDto>(cart);
         }
 
         public async Task<CartDto> GetCartByUserId(string userId)
         {
-            throw new System.NotImplementedException();
         }
 
         public async Task<bool> RemoveFromCart(int cartDetailsId)
         {
-            throw new System.NotImplementedException();
         }
     }
 }
