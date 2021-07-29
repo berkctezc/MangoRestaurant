bfd0574737375dedbd3bd9805793007cce31eb5f
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-07-29 23:57:53 +0300

    created cart repository

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartDto> GetCartByUserId(string userId);
+        Task<CartDto> CreateUpdateCart(CartDto cartDto);
+        Task<bool> RemoveFromCart(int cartDetailsId);
+        Task<bool> ClearCart(string userId);
+    
+        }
+}
